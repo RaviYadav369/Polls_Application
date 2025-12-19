2068ef31b760a0fb2564a83e166a52af3194e7b9
Ravi Yadav <88570593+RaviYadav369@users.noreply.github.com>
2025-12-19 13:02:35 +0000

    update the schema of the database

--- NodeJs/Routes/pollRoutes.js
+++ NodeJs/Routes/pollRoutes.js
@@ -1,13 +1,14 @@
 import express from 'express'
-import Polls from '../database/polls_schema.js'
-const pollsRoute = express.Router()
+import pollDatas from '../database/pollDatas_schema.js'
+const pollDatasRoute = express.Router()
 
-pollsRoute.get('/',async(req,res)=>{
+pollDatasRoute.get('/',async(req,res)=>{
     try {
-        const data = await Polls.find({})
+        const data = await pollDatas.find({})
         if(!data){
-            res.status(400).send({message: "NO Polls data"})
+            res.status(400).send({message: "NO pollDatas data"})
         }
+    
         res.status(200).send(data)
         
     } catch (error) {
@@ -23,13 +24,13 @@ function validate(validString){
     return false
 }
 
-pollsRoute.post('/',async(req,res)=>{
+pollDatasRoute.post('/',async(req,res)=>{
     try {
         const {title,option1,option2,option3,option4} = req.body;
         let data ={}
         if(validate(title) && validate(option1) && validate(option2) && validate(option3) && validate(option4)){
             data={
-                pollsTitle:title,
+                pollDatasTitle:title,
                 option1name :option1,
                 option2name :option2,
                 option3name :option3,
@@ -37,26 +38,45 @@ pollsRoute.post('/',async(req,res)=>{
             }
         }
 
-        await Polls.create(data)
-        req.status(200).send({message:"Polls Created Successful "})
+        await pollDatas.create(data)
+        req.status(200).send({message:"pollDatas Created Successful "})
     } catch (error) {
         res.status(400).send({error:error})
     }
 })
 
-pollsRoute.patch('/polls/:id',async(req,res)=>{
+pollDatasRoute.patch('/pollDatas/:id',async(req,res)=>{
     try {
         const {option} = req.body;
-
-        const pollData = await Polls.findById(req.params.id)
-        if(!pollData){
-            res.status(400).send({message:"NO poll Found"})
+        let dict = {
+            "option1name":1,
+            "option2name":2,
+            "option3name":3,
+            "option4name":4,
         }
-        
+
+        const pollData = await pollDatas.findById(req.params.id)
+        if(!pollData){
+            res.status(400).send({message:"NO pollData Found"})
+        }
+
+        const voteField = `option${dict[option]}vote`;
+        pollData[voteField]+=1;
+
+        const totalVotes = pollData.option1votes + pollData.option2votes + 
+                           pollData.option3votes + pollData.option4votes;
+
+        pollData.option1percentage = Math.round((pollData.option1votes / totalVotes) * 100);
+        pollData.option2percentage = Math.round((pollData.option2votes / totalVotes) * 100);
+        pollData.option3percentage = Math.round((pollData.option3votes / totalVotes) * 100);
+        pollData.option4percentage = Math.round((pollData.option4votes / totalVotes) * 100);
+
+        await pollData.save();
+        res.status(200).send(pollData)
         
     } catch (error) {
      res.status(400).send({error:error})   
     }
 })
 
-export default pollsRoute;
+export default pollDatasRoute;
