79975e2d8031d335be2c173cc746e4b838143a10
Ravi Yadav <88570593+RaviYadav369@users.noreply.github.com>
2025-12-22 13:47:22 +0000

    update code

--- NodeJs/database/polls_schema.js
+++ NodeJs/database/polls_schema.js
@@ -6,14 +6,14 @@ const PollsSchema = new mongoose.Schema({
     option2name :String,
     option3name :String,
     option4name :String,
-    option1vote:{type:number, default:0},
-    option2vote:{type:number, default:0},
-    option3vote:{type:number, default:0},
-    option4vote:{type:number, default:0},
-    option1percentage:{type:number, default:0},
-    option2percentage:{type:number, default:0},
-    option3percentage:{type:number, default:0},
-    option4percentage:{type:number, default:0},
+    option1vote:{type:Number, default:0},
+    option2vote:{type:Number, default:0},
+    option3vote:{type:Number, default:0},
+    option4vote:{type:Number, default:0},
+    option1percentage:{type:Number, default:0},
+    option2percentage:{type:Number, default:0},
+    option3percentage:{type:Number, default:0},
+    option4percentage:{type:Number, default:0},
 })
 
 const Poll = mongoose.model('Poll',PollsSchema);
